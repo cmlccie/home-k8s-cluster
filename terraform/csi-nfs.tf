e9bed163a323c031608c3991e5699ba42a1d9fa6
Chris Lunsford <cm@lunsford.io>
2023-09-24 23:06:44 -0400

    Initial CSI NFS deployment

---
+++ terraform/csi-nfs.tf
@@ -0,0 +1,75 @@
+# -------------------------------------------------------------------------------------------------
+# NFS Subdir External Provisioners (CSI)
+# -------------------------------------------------------------------------------------------------
+
+resource "kubernetes_namespace" "kube_csi_nfs" {
+  metadata {
+    name = "kube-csi-nfs"
+  }
+}
+
+locals {
+  nfs_storage_classes = {
+    "lh-nas01-ssd-rwo" = {
+      nfs_path    = "/ssd-nfs/rwo"
+      access_mode = "ReadWriteOnce"
+      default     = true
+    }
+    "lh-nas01-ssd-rwm" = {
+      nfs_path    = "/ssd-nfs/rwm"
+      access_mode = "ReadWriteMany"
+      default     = false
+    }
+    "lh-nas01-m2-rwo" = {
+      nfs_path    = "/m2-nfs/rwo"
+      access_mode = "ReadWriteOnce"
+      default     = false
+    }
+    "lh-nas01-m2-rwm" = {
+      nfs_path    = "/m2-nfs/rwm"
+      access_mode = "ReadWriteMany"
+      default     = false
+    }
+  }
+}
+
+resource "helm_release" "kube_csi_nfs" {
+  for_each = local.nfs_storage_classes
+
+  name = each.key
+
+  namespace = one(kubernetes_namespace.kube_csi_nfs.metadata[*].name)
+
+  repository = "https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/"
+  chart      = "nfs-subdir-external-provisioner"
+
+  set {
+    name  = "nfs.server"
+    value = "172.28.2.10"
+  }
+
+  set {
+    name  = "nfs.path"
+    value = each.value.nfs_path
+  }
+
+  set {
+    name  = "storageClass.name"
+    value = each.key
+  }
+
+  set {
+    name  = "storageClass.provisionerName"
+    value = "k8s-sigs.io/${each.key}"
+  }
+
+  set {
+    name  = "storageClass.accessModes"
+    value = each.value.access_mode
+  }
+
+  set {
+    name  = "storageClass.defaultClass"
+    value = each.value.default
+  }
+}
