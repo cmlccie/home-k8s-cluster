e27c595f0eca08707ed4f8d383760d5b14c35a95
Chris Lunsford <cm@lunsford.io>
2023-11-03 09:54:52 -0400

    Add NGINX Ingress Controller

--- terraform/cluster/ingress-nginx.tf
+++ terraform/cluster/ingress-nginx.tf
@@ -2,11 +2,20 @@
 # NGINX Ingress Controller
 # --------------------------------------------------------------------------------------
 
-resource "helm_release" "nginx_ingress" {
-  name = "nginx-ingress"
+resource "helm_release" "ingress_nginx" {
+  name = "ingress-nginx"
+
+  namespace        = "kube-ingress-nginx"
+  create_namespace = true
 
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
 
-  values = ["${file("values/nginx_ingress.yaml")}"]
+  values = ["${file("values/ingress-nginx.yaml")}"]
+
+  timeout = 60
+  depends_on = [
+    helm_release.metallb,
+    kubernetes_manifest.metallb_ip_address_pool,
+  ]
 }
