3a7fcd136e914b4eda6cf9355bc374ed2a73c606
Chris Lunsford <cm@lunsford.io>
2023-11-05 23:42:15 -0500

    Refactor nginx ingress to add internal ingress

--- terraform/cluster/kube-ingress-nginx.tf
+++ terraform/cluster/kube-ingress-nginx.tf
@@ -1,17 +1,50 @@
 # --------------------------------------------------------------------------------------
-# NGINX Ingress Controller
+# NGINX Ingress Controllers
 # --------------------------------------------------------------------------------------
 
-resource "helm_release" "ingress_nginx" {
-  name = "ingress-nginx"
+resource "kubernetes_namespace" "kube_ingress_nginx" {
+  metadata {
+    name = "kube-ingress-nginx"
+  }
+}
 
-  namespace        = "kube-ingress-nginx"
-  create_namespace = true
+# --------------------------------------------------------------------------------------
+# Internal Ingress
+# --------------------------------------------------------------------------------------
+
+
+resource "helm_release" "ingress_nginx_internal" {
+  name = "ingress-nginx-internal"
+
+  namespace = one(kubernetes_namespace.kube_ingress_nginx.metadata[*].name)
 
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
 
-  values = ["${file("values/ingress-nginx.yaml")}"]
+  values = ["${file("values/ingress-nginx-internal.yaml")}"]
+
+  timeout = 60
+  depends_on = [
+    helm_release.metallb,
+    kubernetes_manifest.metallb_ip_address_pool,
+  ]
+}
+
+
+# --------------------------------------------------------------------------------------
+# External Ingress
+# --------------------------------------------------------------------------------------
+
+
+resource "helm_release" "ingress_nginx_external" {
+  name = "ingress-nginx-external"
+
+  namespace = one(kubernetes_namespace.kube_ingress_nginx.metadata[*].name)
+
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+
+  values = ["${file("values/ingress-nginx-external.yaml")}"]
 
   timeout = 60
   depends_on = [
